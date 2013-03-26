0ab0f944202d0a651595969812ed8807e6f5e462
Steinbach <steinbach.till@gmx.de>
2013-03-26 16:58:29 +0100

    Vessel

---
+++ src/NMEA/Support/Vessel.cpp
@@ -0,0 +1,9 @@
+//
+//  Vessel.cpp
+//  NMEA-Protocol-Server
+//
+//  Created by Till Steinbach on 26.03.13.
+//
+//
+
+#include "Vessel.h"
---
+++ src/NMEA/Support/Vessel.h
@@ -0,0 +1,14 @@
+//
+//  Vessel.h
+//  NMEA-Protocol-Server
+//
+//  Created by Till Steinbach on 26.03.13.
+//
+//
+
+#ifndef __NMEA_Protocol_Server__Vessel__
+#define __NMEA_Protocol_Server__Vessel__
+
+#include <iostream>
+
+#endif /* defined(__NMEA_Protocol_Server__Vessel__) */
